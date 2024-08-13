a5a8df67eb5454a3b4db22d09a44bd3f93231ba1
Kirk An <kirkan95@gmail.com>
2024-08-13 16:49:18 -0400

    copyright color fix

--- src/Copyright.jsx
+++ src/Copyright.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Copyright = () => {
   return (
-    <div className="text-slate-300">
+    <div className="text-slate-500">
       <p className="text-center text-sm lato-thin pt-10 pb-5">
         Copyright © 2024 Kirkland An
       </p>
